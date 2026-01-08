6d1afa6d6bd1959a88b9090eae4e4e001ed16e3f
Viktor <viktor.avgn@yandex.ru>
2026-01-08 19:02:55 +0700

    универсализация компонентов через пропсы

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,24 @@
-import AddTaskForm from "./AddTaskForm"
-import SearchTaskForm from "./SearchTaskForm"
-import TodoInfo from "./TodoInfo"
-import TodoList from "./TodoList"
+import AddTaskForm from './AddTaskForm'
+import SearchTaskForm from './SearchTaskForm'
+import TodoInfo from './TodoInfo'
+import TodoList from './TodoList'
 
 const Todo = () => {
+  const tasks = [
+    { id: 'task-1', title: 'Купить молоко', isDone: false },
+    { id: 'task-2', title: 'Погладить кота', isDone: true },
+  ]
+
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
       <AddTaskForm />
       <SearchTaskForm />
-      <TodoInfo />
-      <TodoList />
+      <TodoInfo
+        total={tasks.length}
+        done={tasks.filter(({ isDone }) => isDone).length}
+      />
+      <TodoList tasks={tasks} />
     </div>
   )
 }
