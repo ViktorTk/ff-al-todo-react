d9258fccb019ed744bb1e5dd52bac57cc48bf3fc
Viktor <viktor.avgn@yandex.ru>
2026-01-13 00:00:33 +0700

    добавлена работа с состояниями списка задач

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -2,7 +2,7 @@ import Field from './Field'
 import Button from './Button'
 
 const AddTaskForm = (props) => {
-  const { addTask } = props
+  const { addTask, newTaskTitle, setNewTaskTitle } = props
 
   const onSubmit = (event) => {
     event.preventDefault()
@@ -11,7 +11,13 @@ const AddTaskForm = (props) => {
 
   return (
     <form className="todo__form" onSubmit={onSubmit}>
-      <Field className="todo__field" label="New task title" id="new-task" />
+      <Field
+        className="todo__field"
+        label="New task title"
+        id="new-task"
+        value={newTaskTitle}
+        onInput={(event) => setNewTaskTitle(event.target.value)}
+      />
       <Button type="submit">Add</Button>
     </form>
   )
