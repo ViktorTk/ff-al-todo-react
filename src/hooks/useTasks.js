860ad4944b1552d2c3b2baa035f6bc274f452662
Viktor <viktor.avgn@yandex.ru>
2026-01-22 23:26:18 +0700

    начало работы с валидацией формы

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -44,21 +44,19 @@ const useTasks = () => {
     [tasks],
   )
 
-  const addTask = useCallback(() => {
-    if (newTaskTitle.trim().length > 0) {
-      const newTask = {
-        id: crypto?.randomUUID() ?? Date.now().toString(),
-        title: newTaskTitle,
-        isDone: false,
-      }
-      // setTasks([...tasks, newTask])
-      setTasks((prevTasks) => [...prevTasks, newTask])
-      setNewTaskTitle('')
-      setSearchQuery('')
-
-      newTaskInputRef.current.focus()
+  const addTask = useCallback((title) => {
+    const newTask = {
+      id: crypto?.randomUUID() ?? Date.now().toString(),
+      title,
+      isDone: false,
     }
-  }, [newTaskTitle])
+    // setTasks([...tasks, newTask])
+    setTasks((prevTasks) => [...prevTasks, newTask])
+    setNewTaskTitle('')
+    setSearchQuery('')
+
+    newTaskInputRef.current.focus()
+  }, [])
 
   useEffect(() => {
     saveTasks(tasks)
