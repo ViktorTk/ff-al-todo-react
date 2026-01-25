d2cb0b0ff1da31dfd015e8299ef835084cd1b4c3
Viktor <viktor.avgn@yandex.ru>
2026-01-25 20:50:05 +0700

    первичная настойка маршрутизации

---
+++ src/pages/TaskPage.jsx
@@ -0,0 +1,10 @@
+const TaskPage = () => {
+  return (
+    <div>
+      <h1>Детали задачи, страница "TaskPage"</h1>
+      {/* Данные конкретной задачи по id */}
+    </div>
+  )
+}
+
+export default TaskPage
---
+++ src/pages/TasksPage.jsx
@@ -0,0 +1,10 @@
+const TasksPage = () => {
+  return (
+    <div>
+      <h1>Список задачи, страница "TasksPage"</h1>
+      {/* Временная заглушка */}
+    </div>
+  )
+}
+
+export default TasksPage
